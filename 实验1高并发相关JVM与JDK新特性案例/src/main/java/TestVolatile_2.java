f119f39714dd7cbc4a7b0607fe789e5759e866f0
Hanxu2018 <h1076998404@gmail.com>
2021-08-05 10:56:10 +0800

    原子操作

--- 实验1高并发相关JVM与JDK新特性案例/src/main/java/TestVolatile_2.java
+++ 实验1高并发相关JVM与JDK新特性案例/src/main/java/TestVolatile_2.java
@@ -1,22 +1,25 @@
+import java.util.concurrent.atomic.AtomicInteger;
+
 /**
  * @author hanxu (hanxu05@rd.netease.com)
  * @date 2021/8/5 10:40
  */
-public class TestVolatile_1 {
-    public static volatile int num = 0;
+public class TestVolatile_2 {
+    public static AtomicInteger num = new AtomicInteger(0);
 
     public static void main(String[] args) throws InterruptedException {
         for (int i = 0; i < 100; i++) {
             new Thread(new Runnable() {
+                @Override
                 public void run() {
-                    for (int j = 0; j < 10000; j++) {
-                        num++;
+                    for (int j = 0; j < 2000; j++) {
+                        num.incrementAndGet();
                     }
                 }
             }).start();
         }
-        Thread.sleep(3000);
+        Thread.sleep(10000);
 
-        System.out.printf(String.valueOf(num));
+        System.out.printf(num.toString());
     }
 }
